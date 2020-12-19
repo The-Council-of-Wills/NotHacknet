a3c769f9f677a1167dc90778521680f32fa5602d
flamion <laberschleichi@gmail.com>
2020-12-19 14:54:09 +0100

    Added ListDirectory (ls) which lists the contents in the current directory

---
+++ src/main/java/nothacknet/commands/executables/ListDirectory.java
@@ -0,0 +1,29 @@
+package nothacknet.commands.executables;
+
+
+import nothacknet.FileSystem.FileSystemFolder;
+import nothacknet.FileSystem.FileSystemObject;
+import nothacknet.GameState;
+
+import java.util.List;
+
+@Exec(key = "ls")
+public class ListDirectory implements Executable {
+
+    public ListDirectory() {
+
+    }
+
+    @Override
+    public void execute(List<String> args) {
+
+        for (FileSystemObject obj : GameState.getCurrentDirectory().getContents()) {
+            if (obj instanceof FileSystemFolder) {
+                System.out.print("/" + obj.getName() + " ");
+            } else {
+                System.out.print(obj.getName());
+            }
+        }
+        System.out.println();
+    }
+}

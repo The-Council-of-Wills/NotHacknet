6dfca7317f2a1e0067b3b3d7ce2e1ce781312a4e
flamion <laberschleichi@gmail.com>
2020-12-19 14:55:07 +0100

    Signalises a broken initialisation in ReflectionParser

---
+++ src/main/java/nothacknet/commands/commandparser/FailedInitializationNotifier.java
@@ -0,0 +1,17 @@
+package nothacknet.commands.commandparser;
+
+import nothacknet.commands.executables.Executable;
+
+import java.util.List;
+
+public class FailedInitializationNotifier implements Executable {
+
+    public FailedInitializationNotifier() {
+
+    }
+
+    @Override
+    public void execute(List<String> args) {
+        System.out.println("There was an Error during initialization");
+    }
+}
